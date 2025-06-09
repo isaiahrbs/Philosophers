5fa95b9c7b7286c07fbf2421ff8c60eb28ca9ea3
Isaiah Robinson <irobinso@c3r6s5.42lausanne.ch>
2025-06-09 14:04:19 +0200

    fixed validation

--- src/validate_input.c
+++ src/validate_input.c
@@ -3,20 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   validate_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isaiah <isaiah@student.42.fr>              +#+  +:+       +#+        */
+/*   By: irobinso <irobinso@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 16:13:01 by irobinso          #+#    #+#             */
-/*   Updated: 2025/06/03 14:38:19 by isaiah           ###   ########.fr       */
+/*   Updated: 2025/06/09 14:03:56 by irobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include "utils.h"
 
+static int	is_number_too_long(char *str)
+{
+	int	len;
+	int	i;
+
+	i = 0;
+	len = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		len++;
+		i++;
+	}
+	return (len > 10);
+}
+
 static int	check_argument_ranges(int argc, char **argv)
 {
 	int	num_philos;
+	int	i;
 
+	i = 0;
+	while (++i < argc)
+	{
+		if (is_number_too_long(argv[i]))
+		{
+			printf("Error: Argument '%s' is above INTMAX\n", argv[i]);
+			return (1);
+		}
+	}
 	num_philos = ft_atoi(argv[1]);
 	if (num_philos < 1 || num_philos > 200)
 		return (printf("Error: Number of philosophers must be 1-200\n"), 1);
